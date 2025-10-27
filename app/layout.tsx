58b280f0a7c77baf6de8d265b055212626ec6242
CreateConnect <amankwaahtheophilus@gmail.com>
2025-10-27 16:25:38 +0000

    Remove unused Google Analytics script to fix warnings

--- app/layout.tsx
+++ app/layout.tsx
@@ -144,19 +144,6 @@ export default function RootLayout({
         </ThemeProvider>
         <Analytics />
         <SpeedInsights />
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX"></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-XXXXXXXXXX', {
-                page_path: window.location.pathname,
-              });
-            `,
-          }}
-        />
       </body>
     </html>
   )
